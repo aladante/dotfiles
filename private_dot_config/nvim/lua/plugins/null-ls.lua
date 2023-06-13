9e78d77d5eaaab13d5670d00e379adb5b1316055
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-13 13:27:59 +0200

    add google formatter

--- private_dot_config/nvim/lua/plugins/null-ls.lua
+++ private_dot_config/nvim/lua/plugins/null-ls.lua
@@ -1,41 +1,44 @@
-local null_ls = require 'null-ls'
+local null_ls = require("null-ls")
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format {
+  vim.lsp.buf.format({
     filter = function(client)
       -- apply logic to use different servers instead of null-ls here
-      return client.name == 'null-ls'
+      return client.name == "null-ls"
     end,
     bufnr = bufnr,
-  }
+  })
 end
 
-null_ls.setup {
+null_ls.setup({
   sources = {
+    -- FORMATING
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.beautysh,
     null_ls.builtins.formatting.xmlformat,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.hadolint,
-    null_ls.builtins.formatting.prettier.with {
+    null_ls.builtins.formatting.google_java_format,
+    null_ls.builtins.formatting.prettier.with({
       filetypes = {
-        'graphql',
-        'yaml',
+        "graphql",
+        "yaml",
       },
-    },
+    }),
+    -- DIAGNOSTICS
+    null_ls.builtins.diagnostics.hadolint,
   },
   on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     end
   end,
-}
+})

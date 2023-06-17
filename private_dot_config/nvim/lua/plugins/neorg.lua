4d50c50192d508eb88be6cd93d8bf5840a8f0507
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-17 10:47:37 +0200

    17-06-2023 @ 10:47:37 ching

--- private_dot_config/nvim/lua/plugins/neorg.lua
+++ private_dot_config/nvim/lua/plugins/neorg.lua
@@ -1,20 +1,21 @@
-require('neorg').setup {
+require("neorg").setup({
   -- Tell Neorg what modules to load
   load = {
-    ['core.defaults'] = {},                                     -- Load all the default modules
-    ['core.concealer'] = {},                                    -- Allows for use of icons
-    ['core.summary'] = {},                                      -- Allows for links
-    ['core.integrations.treesitter'] = {},                      -- Allows tree sitters
-    ['core.completion'] = { config = { engine = 'nvim-cmp' } }, -- Allows completion
-    ['core.dirman'] = {                                         -- Manage your directories with Neorg
+    ["core.defaults"] = {},                                   -- Load all the default modules
+    ["core.concealer"] = {},                                  -- Allows for use of icons
+    ["core.summary"] = {},                                    -- Allows for links
+    ["core.integrations.treesitter"] = {},                    -- Allows tree sitters
+    ["core.completion"] = { config = { engine = "nvim-cmp" } }, -- Allows completion
+    ["core.dirman"] = {                                       -- Manage your directories with Neorg
       config = {
         workspaces = {
-          home = '~/ToDoList/home/',
-          werk = '~/ToDoList/werk',
+          home = "~/ToDoList/home/",
+          werk = "~/ToDoList/werk",
+          k8s = "~/ToDoList/k8s",
         },
         autodetect = true,
         autochdir = true,
       },
     },
   },
-}
+})

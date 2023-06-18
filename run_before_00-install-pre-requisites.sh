0a5d9799b3dd9981bb59836febe7afc7560344bc
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-18 12:16:25 +0200

    18-06-2023 @ 12:16:25 ching

--- run_before_00-install-pre-requisites.sh
+++ run_before_00-install-pre-requisites.sh
@@ -5,5 +5,5 @@ set -eu
 # Install curl if it's not already installed
 if ! command -v curl >/dev/null; then
     sudo apt update
-    sudo apt install -y curl age
+    sudo apt install -y curl age sudo
 fi

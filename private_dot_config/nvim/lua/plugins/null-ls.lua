4358e88c2bd6c11b3bb1747258e61f7e9dcc5e1f
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-21 11:20:05 +0200

    21-06-2023 @ 11:20:05 ching

--- private_dot_config/nvim/lua/plugins/null-ls.lua
+++ private_dot_config/nvim/lua/plugins/null-ls.lua
@@ -9,36 +9,37 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      -- apply logic to use different servers instead of null-ls here
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply logic to use different servers instead of null-ls here
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 null_ls.setup({
-  sources = {
-    -- FORMATING
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.beautysh,
-    null_ls.builtins.formatting.xmlformat,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.google_java_format,
-    null_ls.builtins.formatting.prettier.with({
-      filetypes = {
-        "graphql",
-        "yaml",
-      },
-    }),
-    -- DIAGNOSTICS
-    null_ls.builtins.diagnostics.hadolint,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    end
-  end,
+	sources = {
+		-- FORMATING
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.formatting.beautysh,
+		null_ls.builtins.formatting.xmlformat,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.google_java_format,
+		null_ls.builtins.formatting.jsonnetfmt,
+		null_ls.builtins.formatting.prettier.with({
+			filetypes = {
+				"graphql",
+				"yaml",
+			},
+		}),
+		-- DIAGNOSTICS
+		null_ls.builtins.diagnostics.hadolint,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		end
+	end,
 })

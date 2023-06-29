e023e5410443169e543d70a3b5efe5e233d90d65
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-29 12:57:21 +0200

    29-06-2023 @ 12:57:21 ching

---
+++ private_dot_config/fish/conf.d/path.fish
@@ -0,0 +1,7 @@
+fish_add_path $HOME/.bin  
+fish_add_path $HOME/.local/bin  
+fish_add_path $HOME/Applications   
+fish_add_path $HOME/.cargo/bin 
+fish_add_path $HOME/go/bin 
+fish_add_path $HOME/.local/bin 
+fish_add_path $fish_user_paths
--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,14 +1,12 @@
 ### ADDING TO THE PATH
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
-set -e fish_user_paths
-set -U fish_user_paths $HOME/.bin  $HOME/.local/bin  $HOME/Applications /var/lib/flatpak/exports/bin/  $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $fish_user_paths
 
 ### EXPORT ###
 set fish_greeting                                 # Supresses fish's intro message
 set TERM "xterm-256color"                         # Sets the terminal type
 set EDITOR "nvim"                                 # $EDITOR use Emacs in terminal
-set VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+set VISUAL "nvim"              # $VISUAL use Emacs in GUI mode
 
 ### "less" as manpager
 set -x MANPAGER "less"

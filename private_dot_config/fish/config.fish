90f0a2a3e0402ed8919c59284e8f865ba66ca311
✝ Terry D Davis ✝ <ja.dorresteijn@basenet.com>
2023-06-29 09:47:22 +0200

    move to fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,7 +2,7 @@
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths
+set -U fish_user_paths $HOME/.bin  $HOME/.local/bin  $HOME/Applications /var/lib/flatpak/exports/bin/  $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $fish_user_paths
 
 ### EXPORT ###
 set fish_greeting                                 # Supresses fish's intro message

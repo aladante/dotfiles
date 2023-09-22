59946968ed58719685f5440874e7c69cdf5b2eaa
l00py <janjr@dorresteijn.nl>
2023-09-22 12:07:31 +0200

    Update .tmux.conf Update .config/fish/completions/tk.fish Update install_packages.sh

--- private_dot_config/fish/completions/tk.fish
+++ private_dot_config/fish/completions/tk.fish
@@ -3,7 +3,7 @@ function __complete_tk
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /usr/local/bin/tk
+    $HOME/go/bin/tk
 end
 complete -f -c tk -a "(__complete_tk)"
 

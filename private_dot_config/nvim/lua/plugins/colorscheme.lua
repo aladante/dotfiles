ecfdc28fd67bc514d35ad654add2e7972f0cd6a2
Jan Dorresteijn <janjr@base.nl>
2023-10-03 13:32:24 +0200

    03-10-2023 @ 13:32:24 ching

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 local cmd = vim.cmd
 -- Default options:
-require('kanagawa').setup {
-  compile = false,  -- enable compiling the colorscheme
-  undercurl = true, -- enable undercurls
-  commentStyle = { italic = true },
-  functionStyle = {},
-  keywordStyle = { italic = true },
-  statementStyle = { bold = true },
-  typeStyle = {},
-  transparent = false,   -- do not set background color
-  dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-  terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  colors = {
-    palette = {},
-    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-  },
-  overrides = function(colors) -- add/modify highlights
-    return {}
-  end,
-  theme = 'lotus', -- Load "wave" theme when 'background' option is not set
-  background = {
-    -- map the value of 'background' option to a theme
-    light = 'lotus',
-    dark = 'dragon', -- try "dragon" !
-  },
-}
-cmd 'colorscheme kanagawa'
+require("kanagawa").setup({
+	compile = false, -- enable compiling the colorscheme
+	undercurl = true, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = true },
+	statementStyle = { bold = true },
+	typeStyle = {},
+	transparent = false, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	colors = {
+		palette = {},
+		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	},
+	overrides = function(colors) -- add/modify highlights
+		return {}
+	end,
+	theme = "lotus", -- Load "wave" theme when 'background' option is not set
+	background = {
+		-- map the value of 'background' option to a theme
+		light = "lotus",
+		dark = "dragon", -- try "dragon" !
+	},
+})
+cmd("colorscheme kanagawa")
